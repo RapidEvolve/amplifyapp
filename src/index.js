0aafa1c389f09b4a208240ef6418529718acc1b2
RapidEvolve <keane.streat@gmail.com>
2020-08-20 17:27:52 -0400

    Configure React project with Amplify resources (Take 2)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,7 +18,3 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
-Amplify.configure(config);
